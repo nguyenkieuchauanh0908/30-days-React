91e736fc490eace32d545607399663cc94a96ac5
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2024-08-18 11:16:39 +0700

    [Day 10] Side effects - useEffect: part 2 - useEffect, clean up function, callback

--- Side Effects useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
+++ Side Effects useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import ProgressBar from "./ProgressBar";
+const TIMER = 3000;
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onCancel();
+    }, TIMER);
+    return () => {
+      clearTimeout(timer);
+    }; //Clean up function;
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -11,6 +23,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
