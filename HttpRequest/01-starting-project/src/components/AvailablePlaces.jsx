7b601416ebde86f8d7e163edf81463f3ff707281
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2024-08-23 13:08:16 +0700

    [Day 14] Custom React Hooks

--- HttpRequest/01-starting-project/src/components/AvailablePlaces.jsx
+++ HttpRequest/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,39 +1,31 @@
-import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
 import { fetchAvailablePlaces } from "../http.js";
+import { useFetch } from "../hooks/useFetch.js";
+
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.latitude,
+        position.longitude
+      );
+      resolve(sortedPlaces);
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        const places = await fetchAvailablePlaces();
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.latitude,
-            position.longitude
-          );
-
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        });
-      } catch (error) {
-        setError({
-          message:
-            error.message ||
-            "Could not fetch places, please try again later...",
-        });
-      }
-    }
-    fetchPlaces();
-  }, []);
+  const {
+    isFetching,
+    error,
+    fetchedData: availablePlaces,
+    setFetchedData: setAvailablePlaces,
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
     return <Error title="An error occured" message={error.message} />;
---
+++ HttpRequest/01-starting-project/src/hooks/useFetch.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+export function useFetch(fetchFn, initialValue) {
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState(initialValue);
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setFetchedData(data);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch data." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, [fetchFn]);
+
+  return {
+    isFetching,
+    fetchedData,
+    setFetchedData,
+    error,
+  };
+}
