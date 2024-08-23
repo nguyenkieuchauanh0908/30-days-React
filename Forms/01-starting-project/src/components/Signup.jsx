b56ca2526a751a49e02e4bdfb073b06a0db9ab0a
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2024-08-23 18:25:43 +0700

    [Day 14] Form and User's Input: practice project - mMixing custom and built-tin validation logic

--- Forms/01-starting-project/src/components/Signup.jsx
+++ Forms/01-starting-project/src/components/Signup.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 export default function Signup() {
+  const [pwNotEqual, setPwNotEqual] = useState(false);
   function handleSubmit(event) {
     event.preventDefault();
     const fd = new FormData(event.target);
     const acquisitionChanel = fd.getAll("acquisition");
     const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionChanel;
+    if (data["password"] !== data["confirm-password"]) {
+      setPwNotEqual(true);
+      return;
+    }
     console.log(data);
     event.target.reset();
   }
@@ -16,13 +22,19 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" required />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            required
+            minLength={6}
+          />
         </div>
 
         <div className="control">
@@ -31,7 +43,11 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            required
           />
+          <div className="control-error">
+            {pwNotEqual && <p>Passwords must match</p>}
+          </div>
         </div>
       </div>
 
@@ -40,12 +56,12 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name" required />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" required />
         </div>
       </div>
 
@@ -90,17 +106,18 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
-          agree to the terms and conditions
+          <input
+            type="checkbox"
+            id="terms-and-conditions"
+            name="terms"
+            required
+          />
+          I agree to the terms and conditions
         </label>
       </div>
 
       <p className="form-actions">
-        <button
-          type="reset"
-          className="button button-flat"
-          onClick={handleReset}
-        >
+        <button type="reset" className="button button-flat">
           Reset
         </button>
         <button type="submit" className="button">
