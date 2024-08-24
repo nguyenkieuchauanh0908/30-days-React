b749aad63d5583a12b948adbf3d967ece841f94e
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2024-08-24 19:43:25 +0700

    [Day 15] Practice project: Food Order App - Create MealItem Component

--- FoodOrderApp/01-starting-project/src/components/Meals.jsx
+++ FoodOrderApp/01-starting-project/src/components/Meals.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import MealItem from "./MealItem";
 export default function Meals() {
   const [loadedMeals, setLoadedMeals] = useState([]);
 
@@ -20,7 +20,7 @@ export default function Meals() {
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => {
-        return <li key={meal.id}>{meal.name}</li>;
+        return <MealItem key={meal.id} meal={meal} />;
       })}
     </ul>
   );
