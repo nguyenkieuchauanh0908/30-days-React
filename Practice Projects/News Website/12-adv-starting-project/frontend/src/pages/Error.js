05604d7bba9968dcc39a7ae07f8a1dd3ac2fad6d
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2024-08-29 10:41:28 +0700

    [Day 20] React Router: Newsletter Website Practice Project - The json() Utility Function

--- Practice Projects/News Website/12-adv-starting-project/frontend/src/pages/Error.js
+++ Practice Projects/News Website/12-adv-starting-project/frontend/src/pages/Error.js
@@ -7,7 +7,7 @@ export default function ErrorPage() {
   let title = "An error occured!";
   let message = "Something went wrong!";
   if (error.status === 500) {
-    message = JSON.parse(error.data).message;
+    message = error.data.message;
   }
   if (error.status === 404) {
     title = "Not found";
--- Practice Projects/News Website/12-adv-starting-project/frontend/src/pages/Events.js
+++ Practice Projects/News Website/12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,29 +1,27 @@
-import { useLoaderData } from "react-router-dom";
-
+import { useLoaderData, json } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
-  if (data.isError) {
-    return <p>{data.message}</p>;
-  } else {
-    const events = data.events;
-    return <>{<EventsList events={events} />}</>;
-  }
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+  const events = data.events;
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/eventsasss");
 
   if (!response.ok) {
-    // return { isError: true, message: "Could not fetch events" };
-    throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
-      status: 500,
-    });
+    // return { isError: true, message: 'Could not fetch events.' };
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500,
+    // });
+    throw json({ message: "Could not fetch events" }, { status: 500 });
   } else {
-    //const resData = await response.json();
     return response; //The loader can automatically parse the response when dev get the data from useLoaderData()
   }
 }
