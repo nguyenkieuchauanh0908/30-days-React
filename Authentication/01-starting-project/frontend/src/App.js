1b6b3c153f546970afc3c42f729ab0505f9ac46e
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2024-08-30 15:13:36 +0700

    [Day 21] Authentication: Practice Project - Implementing the Auth Action

--- Authentication/01-starting-project/frontend/src/App.js
+++ Authentication/01-starting-project/frontend/src/App.js
@@ -14,6 +14,7 @@ import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 import AuthenticationPage from "./pages/Authentication";
+import { action as authAction } from "./pages/Authentication";
 
 const router = createBrowserRouter([
   {
@@ -58,6 +59,7 @@ const router = createBrowserRouter([
       {
         path: "auth",
         element: <AuthenticationPage />,
+        action: authAction,
       },
       {
         path: "newsletter",
